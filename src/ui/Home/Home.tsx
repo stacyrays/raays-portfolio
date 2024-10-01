8632fee0e7b6c05ff2dfa6bcc359ebdcc034690a
Stacy Suits <stacyrays@gmail.com>
2024-09-30 20:10:09 -0700

    Fix some homepage text

--- src/ui/Home/Home.tsx
+++ src/ui/Home/Home.tsx
@@ -15,7 +15,7 @@ const Home = () => {
         <div className="laptop"><LaptopIcon /></div>
         <div className="header-text"> 
           <h1>activate modern solutions</h1>
-          <p>Hi I'm Stace, I'm passionate frontend software engineer and enthusiastic product builder, exhibiting great success leveraging strategic solutions, while delivering high quality code and system architecture.</p>
+          <p>Hi I'm Stace, a passionate Front End Software Engineer and enthusiastic product builder, ready to help you create your next innovative project!</p>
           <div style={{marginTop: '24px'}}>
             <ThemeProvider theme={theme}>
               <Stack gap={2} alignItems="flex-start">
